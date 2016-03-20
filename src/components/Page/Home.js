be4c76a9b53829fd42c28389a69183c700d36c42
Sergey Ivanov <xufocoder@gmail.com>
2016-03-20 17:23:32 +0600

    Add `h2`tag

--- src/components/Page/Home.js
+++ src/components/Page/Home.js
@@ -5,6 +5,7 @@ export default class Home extends React.Component {
   render() {
     return (
       <div>
+        <h2>Home page</h2>
         <p><img src={ BASE_URL + "logo.png"} /></p>
       </div>
     );
