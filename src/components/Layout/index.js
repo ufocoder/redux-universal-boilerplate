eee1ec9224f4818840d06436799862cae09f655f
Sergey Ivanov <xufocoder@gmail.com>
2016-03-21 08:08:16 +0600

    Use `semantic-ui-css`

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,6 +2,9 @@ import React, {PropTypes} from "react";
 import Header from './Header';
 import Footer from './Footer';
 
+import 'semantic-ui-css/components/reset.css';
+import 'semantic-ui-css/components/container.css';
+
 export default class Layout extends React.Component {
   static propTypes = {
     children: PropTypes.object.isRequired
