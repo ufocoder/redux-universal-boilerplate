d5ffa95bf0b34f3e730e46732d2e837b244e1a75
Sergey Ivanov <xufocoder@gmail.com>
2016-03-23 13:58:23 +0600

    Add link to URL `/trends`

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -8,6 +8,7 @@ export default class Header extends React.Component {
         <h1 className="ui dividing header">Redux universal boilerplate</h1>
         <div className="ui secondary pointing menu">
           <IndexLink to="/" className="item" activeClassName="active">Homepage</IndexLink>
+          <Link to="/trends" className="item" activeClassName="active">Github trends</Link>
           <Link to="/about" className="item" activeClassName="active">About</Link>
         </div>
       </div>
