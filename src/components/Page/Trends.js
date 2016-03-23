ede8678f1c32417d0dbc69033eeedb8a7d7bd475
Sergey Ivanov <xufocoder@gmail.com>
2016-03-23 13:57:46 +0600

    Add trends page

---
+++ src/components/Page/Trends.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default class Trends extends React.Component {
+  render() {
+    return (
+      <div>
+        <p>
+          Trends
+        </p>
+      </div>
+    );
+  }
+}
