7989263798a8907cf2df569589be2f86866acb7a
Sergey Ivanov <xufocoder@gmail.com>
2016-03-26 18:37:56 +0600

    Implement route auth redirection behavior

--- src/routes.js
+++ src/routes.js
@@ -3,7 +3,33 @@ import {browserHistory, Router, Route, IndexRoute} from 'react-router';
 import Layout from './components/Layout';
 import NotFound from './components/NotFound';
 import {About, Home, Trends} from './components/Page';
-import {Login} from './components/User';
+import {Login, Logout, Profile} from './components/User';
+import {logout} from './actions/Auth';
+
+function authRequired(store) {
+  return (nextState, replace) => {
+    const state = store.getState();
+    if (!state.auth.loggedIn) {
+      replace('/login');
+    }
+  }
+}
+
+function authNoRequired(store) {
+  return (nextState, replace) => {
+    const state = store.getState();
+    if (state.auth.loggedIn) {
+      replace('/profile');
+    }
+  }
+}
+
+function authLogout(store) {
+  return (nextState, replace) => {
+    store.dispatch(logout());
+    replace('/login');
+  }
+}
 
 export default (store) => {
   return (
@@ -12,7 +38,16 @@ export default (store) => {
         <IndexRoute component={Home} />
         <Route path="about" component={About} />
         <Route path="trends" component={Trends} />
-        <Route path="login" component={Login} />
+
+        <Route onEnter={authNoRequired(store)}>
+          <Route path="login" component={Login} />
+        </Route>
+
+        <Route onEnter={authRequired(store)}>
+          <Route path="profile" component={Profile} />
+          <Route path="logout" onEnter={authLogout(store)} />
+        </Route>
+
         <Route path="*" name="not-found" component={NotFound} />
       </Route>
     </Router>
