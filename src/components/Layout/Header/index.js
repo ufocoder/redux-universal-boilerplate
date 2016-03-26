a2f977e2a2c14e6872250197615e1d8dea09ce8f
Sergey Ivanov <xufocoder@gmail.com>
2016-03-26 16:59:41 +0600

    Add links dependent on `loggedIn`

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -1,16 +1,52 @@
 import React from "react";
 import {IndexLink, Link} from 'react-router';
+import {connect} from 'react-redux';
 
+@connect(
+  state => ({
+    loggedIn: state.auth.loggedIn
+  })
+)
 export default class Header extends React.Component {
   render() {
+
+    let links = [
+      {
+        to: '/trends',
+        title: 'Github trends'
+      },
+      {
+        to: '/about',
+        title: 'About'
+      },
+    ];
+
+    if (! this.props.loggedIn) {
+      links.push({
+        to: '/login',
+        title: 'Login'
+      });
+    } else {
+      links.push({
+        to: '/profile',
+        title: 'Profile'
+      });
+      links.push({
+        to: '/logout',
+        title: 'Logout'
+      });
+    }
+
     return (
       <div className="ui text container" style={{  marginTop: '50px' }}>
         <h1 className="ui dividing header">Redux universal boilerplate</h1>
         <div className="ui secondary pointing menu">
           <IndexLink to="/" className="item" activeClassName="active">Homepage</IndexLink>
-          <Link to="/trends" className="item" activeClassName="active">Github trends</Link>
-          <Link to="/about" className="item" activeClassName="active">About</Link>
-          <Link to="/login" className="item right" activeClassName="active">Login</Link>
+          {
+            links.map(function(link) {
+              return <Link to={link.to} className="item" activeClassName="active">{link.title}</Link>
+            })
+          }
         </div>
       </div>
     );
