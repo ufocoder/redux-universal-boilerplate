a66e954086e490b504385d327930b6f1d02eb87a
Sergey Ivanov <xufocoder@gmail.com>
2016-03-26 17:12:14 +0600

    Use one `semantic-ui` stylesheet file

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,14 +2,7 @@ import React, {PropTypes} from "react";
 import Header from './Header';
 import Footer from './Footer';
 
-import 'semantic-ui-css/components/reset.css';
-import 'semantic-ui-css/components/header.css';
-import 'semantic-ui-css/components/container.css';
-import 'semantic-ui-css/components/segment.css';
-import 'semantic-ui-css/components/menu.css';
-import 'semantic-ui-css/components/message.css';
-import 'semantic-ui-css/components/form.css';
-import 'semantic-ui-css/components/button.css';
+import 'semantic-ui-css/semantic.css';
 
 export default class Layout extends React.Component {
   static propTypes = {
