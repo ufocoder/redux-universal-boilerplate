fa90d69cf2b889c2613beeba3d07619a7bf31548
Sergey Ivanov <xufocoder@gmail.com>
2016-03-27 12:24:04 +0600

    Refactor rename variable

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,12 @@
 import {combineReducers} from 'redux';
 import {routerReducer} from 'react-router-redux';
 import authReducer from './auth';
-import authGithub from './github';
+import githubReducer from './github';
 
 export const rootReducer = combineReducers({
   routing: routerReducer,
   auth: authReducer,
-  github: authGithub
+  github: githubReducer
 });
 
 export default rootReducer;
