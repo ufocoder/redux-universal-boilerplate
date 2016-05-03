b109d5064a2c81776b848951634e15da73b84eea
Sergey Ivanov <xufocoder@gmail.com>
2016-05-03 15:43:42 +0600

    Refactor `Login` contailer. Split layout to two blocks

--- src/common/containers/User/Login.js
+++ src/common/containers/User/Login.js
@@ -37,36 +37,40 @@ export default class Login extends Component {
   }
 
   render() {
-    return this.props.loggedIn
-       ? <div>You already logged in</div>
-       : (
-          <div>
-            <h2>Login form</h2>
 
-            <div className="ui message">
-              For test use this credentials: {TEST_USERNAME}/{TEST_PASSWORD}
-            </div>
+    let userLayout = (
+      <div>You already logged in</div>
+    );
 
-            { this.props.error ?
-              (
-                <Error title="Auth error" message={this.props.error} />
-              ) : null
-            }
-            <form className="ui form" onSubmit={this.handleSubmit}>
-              <div className="field">
-                <label>Username</label>
-                <input type="text" ref="username" placeholder="Enter a username" />
-              </div>
-              <div className="field">
-                <label>Password</label>
-                <input type="password" ref="password" placeholder="Last Name" />
-              </div>
-              <button className="ui button"
-                      type="submit"
-                      onClick={this.handleSubmit}>Submit</button>
-            </form>
+    let guestLayout = (
+      <div>
+        <h2>Login form</h2>
+
+        <div className="ui message">
+          For test use this credentials: {TEST_USERNAME}/{TEST_PASSWORD}
+        </div>
+
+        { this.props.error ?
+          (
+            <Error title="Auth error" message={this.props.error} />
+          ) : null
+        }
+        <form className="ui form" onSubmit={this.handleSubmit}>
+          <div className="field">
+            <label>Username</label>
+            <input type="text" ref="username" placeholder="Enter a username" />
           </div>
-        );
+          <div className="field">
+            <label>Password</label>
+            <input type="password" ref="password" placeholder="Last Name" />
+          </div>
+          <button className="ui button"
+                  type="submit"
+                  onClick={this.handleSubmit}>Submit</button>
+        </form>
+      </div>
+    );
 
+    return this.props.loggedIn ? userLayout: guestLayout;
   }
 }
