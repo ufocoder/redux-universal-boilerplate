99badb8499c8c07d92ed6173d6cf4f3d3a06b3bc
Sergey Ivanov <xufocoder@gmail.com>
2016-05-03 15:50:18 +0600

    Add comment for `NotFound` component

--- src/common/components/NotFound.js
+++ src/common/components/NotFound.js
@@ -7,6 +7,10 @@ export default class NotFound extends React.Component {
       <div>
         <h1>404</h1>
         <p>Page not found</p>
+        <div className="ui info message">
+          This page will return 404 HTTP status
+          if you directly open it in your browser
+        </div>
         <p><Link to="/">Go home</Link></p>
       </div>
     );
