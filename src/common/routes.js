cfdab2aa240cb684fe3432de306b1bd24e11417b
Sergey Ivanov <xufocoder@gmail.com>
2016-05-15 10:27:43 +0600

    Load trend on enter to route

--- src/common/routes.js
+++ src/common/routes.js
@@ -5,15 +5,27 @@ import NotFound from './components/NotFound';
 import {About, Home} from './components/Page';
 import {Login, Profile} from './containers/User';
 import Trends from './containers/Trends';
-import {authRequired, authNoRequired, authLogout} from './helpers/routes';
+import {fetchTrends} from './actions/Github';
+import {
+  authRequired,
+  authNoRequired,
+  authLogout
+} from './helpers/routes';
 
 const routes = store => {
+  /**
+   * Dispatch fetch trends action
+   */
+  function loadTrends() {
+    store.dispatch(fetchTrends());
+  }
+
   return (
     <Router history={browserHistory}>
       <Route path="/" component={Layout}>
         <IndexRoute component={Home} />
         <Route path="about" component={About} />
-        <Route path="trends" component={Trends} />
+        <Route path="trends" component={Trends} onEnter={loadTrends} />
 
         <Route onEnter={authNoRequired(store)}>
           <Route path="login" component={Login} />
