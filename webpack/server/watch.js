cb71a0f7bc0e759d10ead912079ad30ae1f14752
Sergey Ivanov <xufocoder@gmail.com>
2016-06-07 07:59:59 +0600

    Remove useless `proxy` variable

--- webpack/server/watch.js
+++ webpack/server/watch.js
@@ -1,10 +1,6 @@
 var _ = require('lodash');
 var webpack = require('webpack');
 var config = require('./config');
-var proxy = {
-  hostname: process.env.HOSTNAME || 'localhost',
-  port: process.env.PORT || 8000
-};
 
 config.entry.push(
   'webpack/hot/poll?1000'
