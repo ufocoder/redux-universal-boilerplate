7c09dba8633a324a5861dc7d469e50143ea05804
Sergey Ivanov <xufocoder@gmail.com>
2016-08-15 13:52:00 +0600

    Refactor `Trends` container

--- src/common/containers/Trends.js
+++ src/common/containers/Trends.js
@@ -7,49 +7,39 @@ import Error from '../components/Error';
 @connect(
   state => ({
     trends: state.github.trends,
-    loading: state.github.loading,
     error: state.github.error
-  }),
-  dispatch => ({
-    fetchData: () => {
-      dispatch(fetchTrends());
-    }
   })
 )
 export default class Trends extends Component {
   static propTypes = {
     trends: PropTypes.array,
-    loading: PropTypes.bool,
-    error: PropTypes.string,
-    fetchData: PropTypes.func
+    error: PropTypes.string
   }
+
   render() {
+    if (this.props.error) {
+      return (
+        <Error title="Github error" message={this.props.error} />
+      );
+    }
+
     return (
       <div className="ui relaxed divided list">
         <Helmet title="React Github trends" />
 
         <h1>React Github trends</h1>
 
-        { this.props.error ?
-          (
-            <Error title="Github error" message={this.props.error} />
-          ) : null
-        }
-
-        { this.props.loading ?
-          <div>loading..</div> :
-          this.props.trends.map(function(trend, i) {
-            return (
-              <div className="item" key={i}>
-                <i className="large github middle aligned icon"></i>
-                <div className="content">
-                  <a className="header">{trend.full_name}</a>
-                  <div className="description">{trend.stargazers_count}</div>
-                </div>
+        { this.props.trends.map(function(trend, key) {
+          return (
+            <div className="item" key={key}>
+              <i className="large github middle aligned icon"></i>
+              <div className="content">
+                <a className="header">{trend.full_name}</a>
+                <div className="description">{trend.stargazers_count}</div>
               </div>
-            );
-          })
-        }
+            </div>
+          );
+        }) }
       </div>
     );
   }
