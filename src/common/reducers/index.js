d9d6edb6c4008f3ced9ee0773f0d17280c462aa1
Sergey Ivanov <xufocoder@gmail.com>
2016-08-15 13:28:34 +0600

    Refactor reducers index

--- src/common/reducers/index.js
+++ src/common/reducers/index.js
@@ -1,12 +1,12 @@
 import {combineReducers} from 'redux';
-import {routerReducer} from 'react-router-redux';
-import authReducer from './auth';
-import githubReducer from './github';
+import {routerReducer as routing} from 'react-router-redux';
+import auth from './auth';
+import github from './github';
 
 export const rootReducer = combineReducers({
-  routing: routerReducer,
-  auth: authReducer,
-  github: githubReducer
+  routing,
+  auth,
+  github
 });
 
 export default rootReducer;
