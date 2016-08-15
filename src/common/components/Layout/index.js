f8a2489da208fab8abf7027dbde0c720e39fd796
Sergey Ivanov <xufocoder@gmail.com>
2016-08-15 14:06:06 +0600

    Refactor `Layout`

---
+++ src/common/components/Layout/index.js
@@ -0,0 +1,31 @@
+import React, {PropTypes} from "react";
+import {connect} from 'react-redux';
+import Header from './Header';
+import Footer from './Footer';
+
+import 'semantic-ui-css/semantic.css';
+
+export default class Layout extends React.Component {
+  static propTypes = {
+    children: PropTypes.object,
+    loggedIn: PropTypes.bool
+  }
+
+  render() {
+    const {children, loggedIn} = this.props;
+
+    return (
+      <div>
+        <div className="layout-container">
+          <Header loggedIn={loggedIn} />
+          <div className="ui text container">
+            <div className="ui segment">
+              {children}
+            </div>
+          </div>
+        </div>
+        <Footer />
+      </div>
+    );
+  }
+}
--- src/common/containers/Layout/index.js
+++ src/common/containers/Layout/index.js
@@ -1,16 +1,13 @@
 import React, {PropTypes} from "react";
 import {connect} from 'react-redux';
-import Header from './Header';
-import Footer from './Footer';
-
-import 'semantic-ui-css/semantic.css';
+import Layout from '../../components/Layout';
 
 @connect(
   state => ({
     loggedIn: state.auth.loggedIn
   })
 )
-export default class Layout extends React.Component {
+export default class LayoutContainer extends React.Component {
   static propTypes = {
     children: PropTypes.object,
     loggedIn: PropTypes.bool
@@ -18,17 +15,7 @@ export default class Layout extends React.Component {
 
   render() {
     return (
-      <div>
-        <div className="layout-container">
-          <Header loggedIn={this.props.loggedIn} />
-          <div className="ui text container">
-            <div className="ui segment">
-              { this.props.children }
-            </div>
-          </div>
-        </div>
-        <Footer />
-      </div>
+      <Layout {...this.props} />
     );
   }
 }
