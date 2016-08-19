a6c7a1bc1a8cde78871031d6b02a478761c780b5
Sergey Ivanov <xufocoder@gmail.com>
2016-08-19 14:46:29 +0600

    Fix test for `Layout` component

---
+++ tests/unit/components/Layout/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import sinon from 'sinon';
+import {shallow, mount, render} from 'enzyme';
+import Layout from 'src/common/components/Layout';
+import Header from 'src/common/components/Layout/Header';
+import Footer from 'src/common/components/Layout/Footer';
+
+describe("<Layout />", function() {
+  it("contains <Header /> and <Footer /> components", function() {
+    const loggedIn = true;
+    const wrapper = mount(<Layout loggedIn={loggedIn} />);
+
+    expect(wrapper.contains(<Header loggedIn={loggedIn} />)).to.equal(true);
+    expect(wrapper.contains(<Footer />)).to.equal(true);
+  });
+});
