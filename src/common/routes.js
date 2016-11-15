80a1d68b22c7cf041feab781a3cfa2507b40f60f
Sergey <xufocoder@gmail.com>
2016-11-15 22:36:01 +0600

    Modify `routes` according changes

--- src/common/routes.js
+++ src/common/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import {browserHistory, Router, Route, IndexRoute} from 'react-router';
+import {Route, IndexRoute} from 'react-router';
 import Layout from './containers/Layout';
 import NotFound from './components/NotFound';
-import {About, Home} from './components/Page';
-import {Login, Profile} from './containers/User';
+import About from './components/Page/About';
+import Home from './components/Page/Home';
+import Login from './containers/User/Login';
+import Profile from './containers/User/Profile';
 import Github from './containers/Github';
 import {
   authRequired,
@@ -13,24 +15,22 @@ import {
 
 const routes = (store) => {
   return (
-    <Router history={browserHistory}>
-      <Route path="/" component={Layout}>
-        <IndexRoute component={Home} />
-        <Route path="about" component={About} />
-        <Route path="trends" component={Github} />
+    <Route path="/" component={Layout}>
+      <IndexRoute component={Home} />
+      <Route path="about" component={About} />
+      <Route path="trends" component={Github} />
 
-        <Route onEnter={authNoRequired(store)}>
-          <Route path="login" component={Login} />
-        </Route>
-
-        <Route onEnter={authRequired(store)}>
-          <Route path="profile" component={Profile} />
-          <Route path="logout" onEnter={authLogout(store)} />
-        </Route>
-
-        <Route path="*" name="not-found" component={NotFound} />
+      <Route onEnter={authNoRequired(store)}>
+        <Route path="login" component={Login} />
       </Route>
-    </Router>
+
+      <Route onEnter={authRequired(store)}>
+        <Route path="profile" component={Profile} />
+        <Route path="logout" onEnter={authLogout(store)} />
+      </Route>
+
+      <Route path="*" name="not-found" component={NotFound} />
+    </Route>
   );
 };
 
