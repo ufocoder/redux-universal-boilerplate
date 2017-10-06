16c72dfe5e5d7d21ed9ec10e36c6e62afc2ba2fd
Anish Karandikar <anishkny@gmail.com>
2017-10-06 10:20:52 -0400

    test(e2e): Add basic Puppeteer test for Homepage

---
+++ tests/e2e/Homepage.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+const expect = require('chai').expect;
+
+var browser = null;
+var page = null;
+
+describe('Homepage', async function() {
+  before(async() => {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+  });
+
+  after(async() => {
+    await browser.close();
+  });
+
+  it('DOM contains critical elements', async() => {
+    await page.goto('http://localhost:8000/');
+    await page.screenshot({ path: '.screenshots/Homepage.png' });
+    const expectedSelectors = [
+      'div#application',
+      'div.layout-container',
+      'div.ui.text.container',
+      'footer.ui.vertical.footer.segment',
+    ];
+    for (var expectedSelector of expectedSelectors) {
+      expect(await page.$(expectedSelector), `Expected CSS Selector [${expectedSelector}] to exist in DOM`).to.not.be.null;
+    }
+  });
+});
