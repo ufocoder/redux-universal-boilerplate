826f744590a8e91251f2d168dd86abaec6aa4420
Anish Karandikar <anishkny@gmail.com>
2017-10-06 12:03:38 -0400

    test(login): Add e2e test for login/logout

--- tests/e2e/Homepage.js
+++ tests/e2e/Homepage.js
@@ -1,10 +1,10 @@
 const puppeteer = require('puppeteer');
 const expect = require('chai').expect;
 
-let browser = null;
-let page = null;
+describe('Acceptance tests', async function() {
+  let browser = null;
+  let page = null;
 
-describe('Homepage', async function() {
   before(async() => {
     browser = await puppeteer.launch();
     page = await browser.newPage();
@@ -14,7 +14,7 @@ describe('Homepage', async function() {
     await browser.close();
   });
 
-  it('DOM contains critical elements', async() => {
+  it('Homepage DOM contains critical elements', async() => {
     await page.goto('http://localhost:8000/');
     await page.screenshot({path: '.screenshots/Homepage.png'});
     const expectedSelectors = [
@@ -27,5 +27,31 @@ describe('Homepage', async function() {
       expect(await page.$(expectedSelector),
         `Expected CSS Selector [${expectedSelector}] to exist in DOM`).to.not.be.null;
     }
+  });
+
+  it('Login/Logout works', async() => {
+    await page.goto('http://localhost:8000/');
+
+    // Fill out username/password and login
+    await page.click('a.item[href="/login"]');
+    const usernameInputSelector = 'form.ui.form input[placeholder="Enter a username"]';
+    const passwordInputSelector = 'form.ui.form input[type="password"]';
+    await page.waitForSelector(usernameInputSelector);
+    await page.focus(usernameInputSelector);
+    await page.type('demo');
+    await page.focus(passwordInputSelector);
+    await page.type('demo');
+    await page.click('form.ui.form button.ui.button[type="submit"]');
+
+    // Verify we are logged in and expected welcome message is shown
+    await page.waitForSelector('a.item.active[href="/profile"]');
+    await page.screenshot({path: '.screenshots/LoggedIn.png'});
+    let welcomeMessageText = await page.$eval('div.ui.text.container div.ui.segment', (el) => el.innerText);
+    expect(welcomeMessageText).to.equal('Welcome, demo!');
+
+    // Logout
+    await page.click('a.item[href="/logout"]');
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    expect(await page.$('a.item[href="/profile"]')).to.be.null;
   });
 });
